e27b62505b5f5aa5457bc7a06eeabfcd25614fca
Rodrigo Villegas <rodrigovvillegas@gmail.com>
2023-03-14 20:03:55 -0600

    Agregar archivos de proyecto.

---
+++ PlanificadorFCFS/Classes/Proceso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanificadorFCFS.Classes
+{
+    internal class Proceso
+    {
+        int tiempoEjecucion = 0;
+        int tiempoRestante  = 0;
+        public Proceso (int t) 
+        {
+            this.tiempoEjecucion = t;
+            this.tiempoRestante = tiempoEjecucion;
+        }
+    }
+}
