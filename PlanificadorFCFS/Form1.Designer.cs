af201e7c7500a1e9ce2974fb2daca563f363937c
Rodrigo Villegas <rodrigovvillegas@gmail.com>
2023-03-16 20:28:13 -0600

    Se agregó lógica funcional para mover, agregar y detener el movimiento de imágenes, simulando una planificación FCFS

--- PlanificadorFCFS/Form1.Designer.cs
+++ PlanificadorFCFS/Form1.Designer.cs
@@ -30,22 +30,55 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTimer = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.pbProceso = new System.Windows.Forms.ProgressBar();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.pbProceso);
+            this.panel1.Controls.Add(this.lblPrompt);
+            this.panel1.Controls.Add(this.btnTimer);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 450);
             this.panel1.TabIndex = 0;
             // 
+            // btnTimer
+            // 
+            this.btnTimer.Location = new System.Drawing.Point(12, 415);
+            this.btnTimer.Name = "btnTimer";
+            this.btnTimer.Size = new System.Drawing.Size(75, 23);
+            this.btnTimer.TabIndex = 0;
+            this.btnTimer.Text = "Iniciar";
+            this.btnTimer.UseVisualStyleBackColor = true;
+            this.btnTimer.Click += new System.EventHandler(this.btnTimer_Click);
+            // 
             // timer1
             // 
-            this.timer1.Interval = 35000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(485, 420);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(7, 13);
+            this.lblPrompt.TabIndex = 1;
+            this.lblPrompt.Text = "\r\n";
+            // 
+            // pbProceso
+            // 
+            this.pbProceso.Location = new System.Drawing.Point(355, 385);
+            this.pbProceso.Name = "pbProceso";
+            this.pbProceso.Size = new System.Drawing.Size(256, 23);
+            this.pbProceso.TabIndex = 2;
             // 
             // Form1
             // 
@@ -55,7 +88,10 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -63,6 +99,9 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnTimer;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.ProgressBar pbProceso;
     }
 }
 
